ae7a86c93c8f99d60bd3300dee55f3f648b8bf38
kathalsahab <rakesh.kathal@gmail.com>
2021-11-14 20:14:59 +0530

    feat(): Edit, Filter and Search funcationality.

--- my-client/src/App.js
+++ my-client/src/App.js
@@ -2,17 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Layout } from "antd";
 import MovieList from "./components/Movies/MovieList";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import {
-    Form,
-    Input,
-    InputNumber,
-    Button,
-    Modal,
-    DatePicker,
-    Select,
-} from "antd";
+import { Button } from "antd";
+import { RetweetOutlined } from "@ant-design/icons";
 import moment from "moment";
-import { getKeyThenIncreaseKey } from "antd/lib/message";
+import ModalForm from "./components/Movies/Forms/ModalForm";
+import "./App.css";
+import { MySearch } from "./components/Search";
+import { GenreFilter } from "./components/Filter";
+
 const { Header, Content } = Layout;
 
 const config = {
@@ -23,31 +20,30 @@ const App = () => {
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [movies, setMovies] = useState([]);
     const [genres, setGenres] = useState([]);
+    const [edit, setedit] = useState(null);
+    const [loading, setloading] = useState(false);
+    const [genreId, setGenreId] = useState("");
+
+    async function fetchMovies() {
+        let response = await fetch("http://localhost:5000/api/v1/movie");
+        response = await response.json();
+        if (response.success === true) {
+            setMovies(response.data);
+        } else alert(response.error.message);
+    }
+
+    async function fetchGenres() {
+        let response = await fetch("http://localhost:5000/api/v1/movie/genre");
+        response = await response.json();
+        if (response.success === true) {
+            setGenres(response.data);
+        } else alert(response.error.message);
+    }
 
     useEffect(() => {
-        async function fetchMovies() {
-            let response = await fetch("http://localhost:5000/api/v1/movie");
-            response = await response.json();
-            if (response.success === true) {
-                console.log(response);
-                setMovies(response.data);
-            } else alert(response.error.message);
-        }
-
-        async function fetchGenres() {
-            let response = await fetch(
-                "http://localhost:5000/api/v1/movie/genre"
-            );
-            response = await response.json();
-            if (response.success === true) {
-                console.log(response);
-                setGenres(response.data);
-            } else alert(response.error.message);
-        }
-
         fetchGenres();
         fetchMovies();
-    }, [isModalVisible]);
+    }, [loading]);
 
     const showModal = () => {
         setIsModalVisible(true);
@@ -58,140 +54,142 @@ const App = () => {
     };
 
     const handleCancel = () => {
+        setedit(null);
+
         setIsModalVisible(false);
     };
-    const addMovie = (values) => {
-        async function fetchMovies() {
-            let response = await fetch("http://localhost:5000/api/v1/movie", {
-                method: "POST",
-                body: JSON.stringify(values),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            response = await response.json();
-            if (response.success === true) {
-                console.log(response.data);
+    async function addMovie(values) {
+        setloading(true);
+        let response = await fetch("http://localhost:5000/api/v1/movie", {
+            method: "POST",
+            body: JSON.stringify(values),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        response = await response.json();
+        if (response.success === true) {
+            setIsModalVisible(false);
+            setloading(false);
+        } else alert(response.error.payload || response.error.message);
+    }
 
-                setIsModalVisible(false);
-            } else alert(response.error.message || response.error.payload);
-        }
-        fetchMovies();
-    };
+    async function editMovie(values) {
+        setloading(true);
+
+        const editBody = { ...values, movie_id: edit.movie_id };
+        let response = await fetch(`http://localhost:5000/api/v1/movie/edit`, {
+            method: "PUT",
+            body: JSON.stringify(editBody),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.status === 204) {
+            setloading(false);
+
+            setedit(null);
+        } else alert(response);
+    }
 
     const onFinish = (values) => {
-        console.log("Success:", values);
-        console.log(
-            moment(values.release_date).format("YYYY-MM-DDThh:mm:ss[Z]")
-        );
-        values = { ...values, release_date: values.release_date };
-        addMovie(values);
+        values = {
+            ...values,
+            release_date: moment(values.release_date).format(
+                "YYYY-MM-DDThh:mm:ss"
+            ),
+        };
+
+        if (edit) {
+            editMovie(values);
+        } else {
+            addMovie(values);
+        }
     };
 
     const onFinishFailed = (errorInfo) => {
-        console.log("Failed:", errorInfo);
+        alert("Failed:", errorInfo);
     };
 
-    console.log(genres);
+    async function searchMovies(x) {
+        const url = genreId
+            ? `http://localhost:5000/api/v1/movie?title=${x}&genre_id=${genreId}`
+            : `http://localhost:5000/api/v1/movie?title=${x}`;
+
+        let response = await fetch(url);
+        response = await response.json();
+        if (response.success === true) {
+            setMovies(response.data);
+        } else alert(response.error.message);
+    }
+
+    async function filterMovies(genre_id) {
+        let response = await fetch(
+            `http://localhost:5000/api/v1/movie?genre_id=${genre_id}`
+        );
+        response = await response.json();
+        if (response.success === true) {
+            setMovies(response.data);
+        } else alert(response.error.message);
+    }
+
+    const handleSearch = (s) => {
+        searchMovies(s.target.value);
+    };
+    const handleFilter = (s) => {
+        setGenreId(s);
+        filterMovies(s);
+    };
 
     return (
-        <Layout>
-            <Layout className="site-layout">
-                <Header
-                    className="site-layout-background"
-                    style={{ color: "white", padding: 0 }}
+        <section className={"container"}>
+            <header className={"header"}>
+                <h1 className={"title"}>MovieMania</h1>
+                <GenreFilter
+                    filterBy={handleFilter}
+                    className={"action"}
+                    genres={genres}
+                />
+                <Button
+                    type="primary"
+                    shape="circle"
+                    icon={<RetweetOutlined />}
+                    style={{ margin: "0px 8px" }}
+                    onClick={() => {
+                        setGenreId("");
+                        fetchMovies();
+                    }}
+                />
+                <MySearch onSearch={handleSearch} className={"action"} />
+                <Button
+                    type="primary"
+                    onClick={showModal}
+                    style={{
+                        padding: "0 50px",
+                        margin: "16px",
+                        float: "right",
+                    }}
                 >
-                    MovieMania
-                    <Button type="primary" onClick={showModal}>
-                        Primary Button
-                    </Button>
-                </Header>
-                <Content style={{ margin: "64px 0px", padding: "0 50px" }}>
-                    <MovieList movies={movies} />
-                </Content>
-                <Modal
-                    title={"Add Movie"}
-                    visible={isModalVisible}
-                    onOk={handleOk}
-                    onCancel={handleCancel}
-                >
-                    <Form
-                        name="add_movie"
-                        initialValues={{}}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                    >
-                        <Form.Item
-                            label="Movie Title"
-                            name="title"
-                            rules={[
-                                {
-                                    required: true,
-                                    message:
-                                        "Title must be between 3-60 characters",
-                                    min: 3,
-                                    max: 60,
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-                        <Form.Item
-                            name="realease_date"
-                            label="Release Date"
-                            {...config}
-                        >
-                            <DatePicker />
-                        </Form.Item>
-
-                        <Form.Item name="genre_id" label="Select">
-                            <Select>
-                                {genres.map((g) => (
-                                    <Select.Option value={g.genre_id}>
-                                        {g.genre_name}
-                                    </Select.Option>
-                                ))}
-                                {/* <Select.Option value="demo">Demo</Select.Option> */}
-                            </Select>
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Rating"
-                            name="rating"
-                            rules={[
-                                {
-                                    type: "number",
-                                    message: "Please enter a decimal value",
-                                },
-                            ]}
-                        >
-                            <InputNumber type="number" min={0} max={10} />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Price"
-                            name="price"
-                            rules={[
-                                {
-                                    type: "number",
-                                    message: "Please enter a decimal value",
-                                },
-                            ]}
-                        >
-                            <InputNumber type="number" min={0} />
-                        </Form.Item>
-
-                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                            <Button type="primary" htmlType="submit">
-                                Submit
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </Modal>
-            </Layout>
-        </Layout>
+                    Add Movie
+                </Button>
+            </header>
+            <MovieList
+                movies={movies}
+                setedit={setedit}
+                setloading={setloading}
+            />
+            <ModalForm
+                isModalVisible={isModalVisible}
+                edit={edit}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                config={config}
+                genres={genres}
+                handleCancel={handleCancel}
+                handleOk={handleOk}
+            />{" "}
+        </section>
     );
 };
 
